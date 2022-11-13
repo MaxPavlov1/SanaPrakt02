1c44bed0d7886da58f54404d8c4d2885bc9b6475
MaxymPavlov <kn212_pmv@student.ztu.edu.ua>
2022-11-13 21:37:54 +0200

    Довільний текст у перших 3-ох рядках

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Довільний текст у 1 рядку");
+Console.WriteLine("Довільний текст у 2 рядку");
+Console.WriteLine("Довільний текст у 3 рядку");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
